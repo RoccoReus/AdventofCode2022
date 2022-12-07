a5888532234e5bf838bc15619db584e02cadb142
RoccoReus <75258357+RoccoReus@users.noreply.github.com>
2022-12-07 15:03:48 +0100

    Day7 of Adventofcode 2022

---
+++ day7.py
@@ -0,0 +1,54 @@
+with open("day7.txt") as f:
+    wordlist = [line.rstrip() for line in f]
+
+splitter = "/"
+directsplit = [splitter]
+fullList = {splitter: 0}
+allnumbers = 0
+
+
+for x in wordlist:
+    checkstring = x.split(" ")
+    if [w for w in x.split()[1:] if x.startswith("$")]:
+        if checkstring[2 - 1] == "cd":
+            if checkstring[3 - 1] == "/":
+                directsplit = [splitter]
+            if checkstring[3 - 1] == "..":
+                directsplit.pop()
+            else:
+                directsplit.append(checkstring[3 - 1])
+                fullList[splitter.join(directsplit)] = 0
+        elif checkstring[2 - 1] == "ls":
+            #print(fullList)
+            continue
+    elif [w for w in x.split()[1:] if x.startswith("dir")]:
+        continue
+    else:
+        fullList[splitter.join(directsplit)] += int(checkstring[0])
+        allnumbers += int(checkstring[0])
+
+        if len(directsplit) == 1:
+            fullList[splitter] += int(checkstring[0])
+        else:
+            for x in range(len(directsplit) -1, 0, -1):
+                fullList[splitter.join(directsplit[:x])] += int(checkstring[0])
+
+number = 70000000 - allnumbers
+mini = 30000000
+temp = []
+awnser = 0
+
+
+for x in fullList.values():
+    if x <= 100000:
+        awnser += x
+
+for x in fullList.values():
+    if number + x >= mini:
+        temp.append(x)
+
+awnser1 = awnser
+awnser2 = min(temp)
+
+print(awnser1)
+print(awnser2)
